02702b6a8e94e88fe7e21ae1b4db53b3e09be7b2
IsaacRid <isaac.rideal@gmail.com>
2025-10-22 12:01:32 +0100

    Debugging

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+console.log('MONGODB_URI=', process.env.MONGODB_URI);
 
 export const connectDB = async () => {
     try {
